882fd4be689435093d16aa6e26debf88887c81b7
ABHISHEK JAISWAR <141838097+jaiswarabhishek638@users.noreply.github.com>
2026-07-16 00:15:44 +0530

    Add files via upload

---
+++ Day1/firstcode.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+}
+
+// Notation
+/* 1. #include<iostream> //header file --->file jisme inbuid program or function hota hai/ defined hota hai (fn) such AS ""std"" and ""main()"" fn */ 
+
+/* 2. using namespace std; //ek file jisme cin, cout, vector jaise commands defined hota hai;*/
+
+/* 3. int main(){}
+// main() is a function jo sabse pahela run hota hai program , without main ke program start nhi hoga;*/
